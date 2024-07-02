81ae394a2cd71692c5713ac09b06b578cbef5b24
Most Khal <mostafa6khaled@gmail.com>
2024-07-03 00:36:23 +0300

    updating main.cpp to try image create with font

--- main.cpp
+++ main.cpp
@@ -7,24 +7,13 @@ const int font_size = 48;
 
 int main(int argc, const char* argv[])
 {
-  std::string str("hello");
+  std::wstring str(L" hello world");
   gfx::font font;
-  font.load("/usr/share/fonts/truetype/hack/Hack-Regular.ttf");
+  font.load("/usr/local/share/fonts/HackNerdFont/HackNerdFont-Regular.ttf");
   font.set_size(0, font_size);
 
-  gfx::image atlas;
-  atlas.create(str.size() * font_size, font_size, 1);
-
-  int pen = 0;
-  for(int i = 0; i < str.size(); ++i)
-  {
-    auto ch = font.get(str[i]);
-    gfx::image c;
-    c.create(ch.w, ch.h, 1, ch.data);
-    atlas.overlay(c, pen , 0);
-    pen += ch.advance;
-  }
-
-  atlas.save("test.jpg");
+  gfx::image img;
+  img.create(str, font);
+  img.save("test.jpg");
 }
 
